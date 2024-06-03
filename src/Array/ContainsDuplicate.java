745ee4da73536faa140ee0c1385d5ac353e0ea51
Ankit Shekam <60414930+ankit-shekam@users.noreply.github.com>
2024-06-03 22:40:49 +0530

    3 june checkin - 3Q

--- src/Array/ContainsDuplicate.java
+++ src/Array/ContainsDuplicate.java
@@ -1,25 +1,28 @@
-//Given an integer array nums, return true if any value appears at least twice in the array,
-//and return false if every element is distinct.
-//Example 1 : => Input: nums = [1,2,3,1] Output: true
-//Example 2: => Input: nums = [1,2,3,4] Output: false
-//Example 3: =>  Input: nums = [1,1,1,3,3,4,3,2,4,2] Output: true
-//
-//Constraints:
-//        1 <= nums.length <= 10^(5)
-//        -10^(9) <= nums[i] <= 10^(9)
 package Array;
-
 
 import java.util.*;
 import java.lang.*;
 import org.jetbrains.annotations.NotNull;
 
 public class ContainsDuplicate {
+/***
+ * //Given an integer array nums, return true if any value appears at least twice in the array,
+ * //and return false if every element is distinct.
+ * //Example 1 : => Input: nums = [1,2,3,1] | Output: true
+ * //Example 2: => Input: nums = [1,2,3,4] | Output: false
+ * //Example 3: =>  Input: nums = [1,1,1,3,3,4,3,2,4,2] | Output: true
+ * //Constraints:
+ * //1 <= nums.length <= 10^(5)
+ * //-10^(9) <= nums[i] <= 10^(9)
+ */
   public static boolean containsDuplicate(int @NotNull [] nums){
-    Set<Integer> hset = new HashSet<>(); //sets dont allow duplicates
-    for(int num:nums){
-      if(!hset.add(num)) return true; //tried to insert duplicate element into the set
+    // sort - Space & time = O(NlogN)
+    // use hashset - space & Time = O(N)
+    // Till 10^6 -> O(NlogN) is OK
+    Set<Integer> hset = new HashSet<>();
+    for(int num : nums){
+      if(!hset.add(num)) return true; //tried inserting duplicate in the set
     }
-    return false;
+    return false; //no duplicates
   }
 }
--- src/Array/ValidAnagram.java
+++ src/Array/ValidAnagram.java
@@ -4,4 +4,32 @@ import java.util.*;
 import java.lang.*;
 import org.jetbrains.annotations.NotNull;
 public class ValidAnagram {
+/***
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ * typically using all the original letters exactly once.
+ * Example 1:Input: s = "anagram", t = "nagaram" | Output: true
+ * Example 2: Input: s = "rat", t = "car" | Output: false
+ * Constraints:
+ * 1 <= s.length, t.length <= 5 * 104
+ * s and t consist of lowercase English letters.
+ */
+  public static boolean isAnagram(@NotNull String s, @NotNull String t) {
+    // sort both and incremental compare - time & space = O(NlogN)
+    // use HashMap - time and space = O(N)
+    // use freqArray since only small eng letters - space ~ O(1), time O(N)
+    int[] freqArr = new int[26];
+    for(int i=0; i<s.length(); i++){
+      char val = s.charAt(i);
+      freqArr[val-'a']++;
+    }
+    for(int i=0; i<t.length(); i++){
+      char val = t.charAt(i);
+      freqArr[val-'a']--;
+    }
+    for(int num : freqArr){
+      if(num!=0) return false;
+    }
+    return true;
+  }
 }
